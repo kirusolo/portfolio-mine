0f299e76549026d1babc911e7818999eb1192f95
kirusolo <kirubelendala15@gmail.com>
2023-09-18 23:11:16 +0200

    put my page arangment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Projects from "./Pages/Projects";
+import Technologies from "./Pages/Technologies";
 function App() {
   return (
     <Router>
---
+++ src/Pages/Home.js
@@ -0,0 +1,109 @@
+import React, { useRef, useEffect, useState } from "react";
+import gsap from "gsap";
+import { personalDetails } from "../Details";
+
+function Home() {
+  const { name, tagline, img } = personalDetails;
+  const h11 = useRef();
+  const h12 = useRef();
+  const h13 = useRef();
+  const myimageref = useRef();
+
+  // State to track dark mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(
+      h11.current,
+      {
+        x: "-100%",
+        delay: 0.8,
+        opacity: 0,
+        duration: 2,
+        ease: "Power3.easeOut",
+      },
+      "<"
+    )
+      .from(
+        h12.current,
+        {
+          x: "-100%",
+          delay: 0.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        h13.current,
+        {
+          x: "-100%",
+          delay: 0.1,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        myimageref.current,
+        {
+          x: "200%",
+          delay: 1.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      );
+  }, []);
+
+  // Function to toggle dark mode
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <main
+      className={`container mx-auto max-width section md:flex justify-between items-center ${
+        darkMode ? "dark-mode" : ""
+      }`}
+    >
+      <div>
+        <h1
+          ref={h11}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          Hi,👋<br></br>My Name is<br></br>
+        </h1>
+        <h1
+          ref={h12}
+          className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          {name}
+        </h1>
+        <h2
+          ref={h13}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          {tagline}
+        </h2>
+      </div>
+      <div className="mt-5 md:mt-0">
+        <img
+          ref={myimageref}
+          className="w-1/2 md:ml-auto rounded-full" // Apply rounded-full class for circular shape
+          src={img}
+          alt="Kirubel Baye"
+        />
+      </div>
+      <button onClick={toggleDarkMode}>
+        {darkMode ? "Dark Mode On 🔦" : "Dark Mode Off ☀️"}
+      </button>
+    </main>
+  );
+}
+
+export default Home;
